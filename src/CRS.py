82e37e7e649f1c8bf643a2c410ddb577032a853c
baoj3101 <38962382+baoj3101@users.noreply.github.com>
2019-07-29 12:01:55 -0700

    Update CRS.py

--- src/CRS.py
+++ src/CRS.py
@@ -1,45 +1,100 @@
 #!/usr/bin/python3
 
+'''
+Parse sparse matrix from data file
+'''
 import re
+import argparse
 
-fn = './bcsstk15/bcsstk15.mtx'
+#=================================================================================
+# Utility functions
+#=================================================================================
+def debug(matrix):
+    for col in matrix:
+        for cell in col:
+            print (cell[0], cell[1], cell[2])    
 
-in_fh = open(fn, 'r')
-out_fh = open('log', 'w')
+def splitLine (line):
+    toks = line.split(' ')
+    return [int(toks[0]), int(toks[1]), float(toks[2])]
 
-M = 0
-N = 0
-NUM = 0
-VAL = []
-COL = []
-PTR = []
-p = 0
-tmp = []
-
-for line in in_fh:
-    line = line.strip()
-    if re.match("^\%", line):
-        continue
-    
-    if NUM == 0:
-        toks = line.split(' ')
-        M = int(toks[0])
-        N = int(toks[1])
-        NUM = int(toks[2])
-        
-    else:
-        toks = line.split(' ')
-        i = int(toks[0])
-        j = int(toks[1])
-        #print("[" + line + "]\n")
-        VAL.append(int(toks[2]))
-        p+=1
-        COL.append(j)
-        if i in tmp:
+#=================================================================================
+# Read input data from file
+#=================================================================================
+NUM_ROWS = 0
+def readInput(in_file):
+    fh = open (in_file)
+    # get 1st line matrix size
+    M = 0
+    for line in fh:
+        # skip comments
+        if re.match("\%", line):
             continue
-        else:
-            PTR.append(p)
-            tmp.append(i)
-                        
-in_fh.close()
-out_fh.close()
+    
+        if M == 0:                                   # 1st line of data size M N TOTAL
+            cell = splitLine(line)
+            M = cell[1]
+            NUM_ROWS = M
+            matrix = [[] for i in range(M + 1)]      # *** index starts from 1
+            break
+
+    # read data from file
+    for line in fh:
+        cell = splitLine(line)
+        i = cell[0]
+        j = cell[1]
+        matrix[i].append(cell)                      # *** add to row MATRIX[i]
+        if i > j:
+            matrix[j].append([j, i, cell[2]])       # *** add symmetric value if not i == j
+    
+    fh.close()
+    return matrix
+
+#=================================================================================
+# Put data into VAL/ROW/PTR
+#=================================================================================
+def storeData(matrix, out_file):
+    VAL = []
+    COL = []
+    PTR = [0]*len(matrix)
+    PTR[0] = 0
+
+    for row in matrix:                              # row is a list of one matrix column non-zero data
+        for cell in row:
+            i = cell[0]
+            j = cell[1]
+            VAL.append(cell[2])                     # put data into VAL/ROW/PTR
+            COL.append(j - 1)
+            PTR[i] += 1
+             
+            
+    # process PTR
+    for i in range(1, len(PTR)):              #******!!!!!! change from 2 -> 1
+        PTR[i] += PTR[i-1]
+
+    # output
+    import sys
+    with open(out_file, 'w') as fh:
+        sys.stdout = fh
+        print ("%BEGIN PTR", len(PTR))
+        for val in PTR:
+            print (val)
+        print ("%BEGIN VAL", len(VAL))
+        for val in VAL:
+            print (val)
+        print ("%BEGIN COL", len(COL))
+        for val in COL:
+            print (val)
+        
+            
+#=================================================================================
+# Main
+#=================================================================================
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Sparse Matrix Parser')
+    parser.add_argument('--input', type=str, help='input data file')
+    args = parser.parse_args()
+  
+    matrix = readInput(args.input)
+    debug(matrix)
+    storeData(matrix, args.input + ".out")
